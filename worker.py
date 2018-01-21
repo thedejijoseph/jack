197e2492954000e9efb9463559e9e4c7e4118eb2
Deji Joseph <wrecodde@gmail.com>
2018-01-21 13:56:19 +0100

    straightened out worker.py
    + added containered functions
    + worker.py closes when work's done now

--- worker.py
+++ worker.py
@@ -10,6 +10,7 @@ import threading
 import logging
 import queue
 import json
+import time
 
 from itertools import permutations as scramble
 
@@ -19,14 +20,12 @@ logging.basicConfig(
 	format = "%(asctime)s | %(levelname)s | %(message)s"
 )
 
-source_file = open("assets/2.txt", "r")
-save_file = open("assets/saved.txt", "r")
-words_file = open("assets/words.txt", "r")
+def is_undone(item):
+	return True if item not in done else False
 
 def load_cache(cache_file):
 	# where cache_file is a txt file
 	# return a dict of its content
-	
 	cache = {}
 	content = cache_file.splitlines()
 	
@@ -35,23 +34,6 @@ def load_cache(cache_file):
 		cache[item] = json.loads(entry)
 	
 	return cache
-
-def is_undone(item):
-	return True if item not in done else False
-
-logging.debug("setting up")
-
-source = source_file.read().splitlines()
-saved = load_cache(save_file.read())
-all_words = set(words_file.read().splitlines())
-
-done = saved.keys()
-undone = [i for i in filter(is_undone, source)]
-
-# now we have a list of items left to work on
-source_file.close()
-save_file.close() # to be opened again for writing
-words_file.close()
 
 def process(word):
 	# get all possible words from this word
@@ -71,48 +53,98 @@ def process(word):
 
 def worker():
 	while True:
-		item = work_queue.get()
-		if item is None:
+		if work_queue.empty() is True:
 			break
 		
-		logging.debug(f"work: {item}")
-		entry = process(item)
-		package = item + ": " + json.dumps(entry)
-		save_queue.put(package)
-		logging.debug(f"done: {item}")
+		else:
+			item = work_queue.get()
+			
+			logging.info(f"work: {item}")
+			entry = process(item)
+			package = item + ": " + json.dumps(entry)
+			save_queue.put(package)
+			logging.info(f"done: {item}")
+			
+			work_queue.task_done()
+
+def save():
+	# all the work left to do has been queued
+	# work has started and results are being queued
+	# start saving!
+
+	while True:
+		if save_queue.empty() is False:
+			package = save_queue.get()
+			item = package.split(': ')[0]
+			logging.info(f"save: {item}")
+			
+			# handled this way for assurance that
+			# items saved (written to file) are saved
+			
+			save_file = open("assets/saved.txt", "a")
+			save_file.write(package + "\n")
+			save_file.close()
 		
-		work_queue.task_done()
+		elif save_queue.empty() is True:
+			# check if all the work is done
+			if work_queue.empty():
+				# check again if all the work's been saved
+				time.sleep(3)
+				if save_queue.empty():
+					# if this checks out, workers
+					# would have stopped
+					# close the threads!
+					for wrkr in workers:
+						wrkr.join()
+					
+					break
 
-work_queue = queue.Queue()
-save_queue = queue.Queue()
-
-# start workers
-no_of_workers = 16
-workers = []
-
-logging.debug("queuing work")
-
-for item in undone:
-	work_queue.put(item)
-
-for i in range(no_of_workers):
-	wrkr = threading.Thread(target=worker)
-	wrkr.start()
+def start():
+	global done, undone, all_words
+	global work_queue, save_queue
+	global no_of_workers, workers
 	
-	workers.append(wrkr)
+	words_file = open("assets/words.txt", "r")
+	all_words = set(words_file.read().splitlines())
+	words_file.close()
+	
+	source_file = open("assets/1.txt", "r")
+	save_file = open("assets/saved.txt", "r")
+	
+	logging.info("setting up")
+	source = source_file.read().splitlines()
+	saved = load_cache(save_file.read())
+	
+	done = saved.keys()
+	undone = [i for i in filter(is_undone, source)]
+	
+	if undone == []:
+		logging.info("all clear!")
+		return
 
-# all the work left to do has been queued
-# work has started and results are being queued
-# start saving!
+	# now we have a list of items left to work on
+	source_file.close()
+	save_file.close() # to be opened again for writing
+	
+	# open up queue objects
+	work_queue = queue.Queue()
+	save_queue = queue.Queue()
+	
+	# start workers
+	no_of_workers = 16
+	workers = []
 
-while True:
-	if save_queue.empty() is False:
-		package = save_queue.get()
-		logging.debug(f"save: {package.split(': ')[0]}")
+	logging.info("queuing work")
+	for item in undone:
+		work_queue.put(item)
+
+	for i in range(no_of_workers):
+		wrkr = threading.Thread(target=worker)
+		wrkr.start()
 		
-		# handled this way for assurance that
-		# items saved (written to file) are saved
-		
-		save_file = open("assets/saved.txt", "a")
-		save_file.write(package + "\n")
-		save_file.close()
+		workers.append(wrkr)
+	
+	save()
+
+if __name__ == "__main__":
+	start()
