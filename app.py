266cef7d1aa347530f3b41969b3b38124ef43959
Deji Joseph <wrecodde@gmail.com>
2018-02-01 08:41:37 +0100

    core app.py lokks ready to fly
    compartmentalization: 50% achieved
    grace.

--- app.py
+++ app.py
@@ -1,32 +1,12 @@
 # app.py: hold app resource
 
 
-import logging
-import itertools
 import types
 import shutil
+import time
 
-# setup
-# -----
-
-scramble = itertools.permutations
-
-logging.basicConfig(
-	level = logging.DEBUG,
-	format = "%(asctime)s | %(levelname)s | %(message)s"
-)
-
-try:
-	word_file_path = "assets/words.txt"
-	with open(word_file_path) as words_file:
-		words = words_file.read().splitlines()
-		word_bank = set(words)
-except:
-	logging.warning(f"file not found: {word_file_path}")
-	
-	import sys
-	sys.exit()
-
+from config import *
+from caching import *
 
 # core methods
 # ------------
